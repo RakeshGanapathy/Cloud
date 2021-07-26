48e33a713d7900eed28b6eaeabcc32cfa240ef24
Rakesh Ganapathy <rakesh.ganapathy@gmail.com>
2021-07-26 15:54:42 +0530

    Update commands.sh

--- ShellScripts/commands.sh
+++ ShellScripts/commands.sh
@@ -1,1 +1,3 @@
 aws ec2 describe-instances --filters Name=instance-state-name,Values=running
+
+docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) (accountNo).dkr.ecr.ap-south-1.amazonaws.com
